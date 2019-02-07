143ee49112d29f2cda2938a3f0f1b3b2259785b6
dugganaa <dugganaa@tcd.ie>
2019-02-07 17:30:06 +0000

    Assignment 1 completed

--- ex1/gradientDescentMulti.m
+++ ex1/gradientDescentMulti.m
@@ -16,7 +16,10 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
+    h0 = X * theta;
+    for i=1:size(X,2)
+    theta(i) = theta(i) - (alpha / m) .* sum((h0 - y)' * X(:,i));
+    end
 
 
 
